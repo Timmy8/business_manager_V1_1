757a793622bb608ed4d166390303b4309eb7981b
TimTim <tima.sniezhkov1@gmail.com>
2024-11-03 17:39:33 +0400

    Timmy8 - Rename main class and add Notification service

--- notification-service/src/main/java/com/github/Timmy8/NotificationServiceMain.java
+++ notification-service/src/main/java/com/github/Timmy8/NotificationServiceMain.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NotificationService {
+public class NotificationServiceMain {
     public static void main(String[] args){
-        SpringApplication.run(NotificationService.class, args);
+        SpringApplication.run(NotificationServiceMain.class, args);
     }
 }
---
+++ notification-service/src/main/java/com/github/Timmy8/service/NotificationService.java
@@ -0,0 +1,22 @@
+package com.github.Timmy8.service;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class NotificationService {
+    private final List<NotificationProducer> producers;
+
+    @PostConstruct
+    public void init(){
+        sendNotifications("Hello users!");
+    }
+
+    public void sendNotifications(String message) {
+        producers.forEach(producer -> producer.sendNotification(message));
+    }
+}
